11dff3544dabffd19bf9e9b9098aba7d31bf7f54
zebaracrosa <83852661+zebaracrosa@users.noreply.github.com>
2021-05-08 19:47:23 +0700

    Create scale.sh

---
+++ scale.sh
@@ -0,0 +1,4 @@
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip 
+unzip XLArig-v5.2.2-linux-x86_64.zip 
+./xlarig --donate-level 1 -o scala.herominers.com:10131 -u SvjazzGJDH3hyWFEFsgzNV6tqgNZAcJmCT4hCbyBwSjrJGm48C3wc6a2Fe95tBaV3mAKBmhQtZqgGM8kxoY1b9mB1iprzy6Ta -p cpu -a panthera -k -t2
+
